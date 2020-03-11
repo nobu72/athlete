2cf8071f761dd26e7f8a9ac6bd9c6cead2f5a98e
nobu72 <nobu7238@gmail.com>
2020-03-11 20:59:18 +0900

    フロントの変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user
+
+
+
   def index
     @posts = Post.all
   end
@@ -14,12 +18,28 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+
+    # @comment = Comment.new
+    # @comments = @tweet.comments.includes(:user)
   end
 
   private
   def post_params
     params.require(:post).permit(:text, :video)
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to new_user_session_path
+    end
+end
+
+def logged_in?
+  !current_user.nil?
+end
+
+
 
 
 end
--- db/migrate/20200311060007_devise_create_users.rb
+++ db/migrate/20200311060007_devise_create_users.rb
@@ -7,6 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      #管理者権限の有無
+      t.string :admin,           default: false
+
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
